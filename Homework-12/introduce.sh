5f9748ff93eac8f0b59e671ca78d9c99b1f73ca5
EC2 Default User <ec2-user@ip-172-31-83-51.ec2.internal>
2025-06-29 00:54:38 +0000

    Create varibales

---
+++ Homework-12/introduce.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+name=Asel
+age=44
+favorite_language=english
+hobby=planting_flowers
+dream=become_the_best_DevOps_engineer
+
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time $hobby.
+
+As a $age year old, I believe it's never too late to follow your dreams. My favorite programming language is $favorite_language.
+
+I code in $favorite_language almost every day!
+
+One day, I hope my love for $favorite_language will help me achieve my dream of $dream."
+
+echo "When I'm not busy working toward $dream, you'll find me $hobby to relax."
+
+echo "That's a little about me, $name a $age year old who loves $favorite_language, enjoys $hobby, and dreams of $dream!"
